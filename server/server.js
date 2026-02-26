484f952069b51b2b6163897413aeaa5167086fa0
carthick0 <karthikvempala31@gmail.com>
2026-02-26 13:02:50 +0530

    Added deployed backend url in the frontend

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 
 // Enable CORS
 app.use(cors({
-    origin: 'http://localhost:5173', // Vite default port
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true
 }));
 
